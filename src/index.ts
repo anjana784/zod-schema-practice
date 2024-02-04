27f1a8c8cd6565220dfe095cefe5ba3278c2b99d
anjana784 <hello@anjana784.dev>
2024-02-04 21:16:56 +0530

    used omit utility function to removed mainCategory and subCategory from the infered type from AdSchema

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,54 @@
-import { z } from "zod";
-import { CategorySchema, CategoryType } from "./schema/Category";
-import { AdSchema } from "./schema/Ad";
+import { AdSchema, VehicleType, PropertyType, AdType } from "./schema/Ad";
 
-const propertySubCategories = ["house", "apartment"] as const;
-const vehiclesSubCategories = ["car", "motorcycle", "truck"] as const;
-
-type propertySubCategoriesType = (typeof propertySubCategories)[number];
-type vehiclesSubCategoriesType = (typeof vehiclesSubCategories)[number];
-
-type AdType = { title: string } & (
-  | { mainCategory: "vehicles"; subCategory: vehiclesSubCategoriesType }
-  | { mainCategory: "property"; subCategory: propertySubCategoriesType }
-);
-
-const data: AdType = {
-  title: "House for sale",
-  mainCategory: "vehicles",
-  subCategory: "truck",
+const car: VehicleType = {
+  mainCategory: "vehicle",
+  subCategory: "car",
+  title: "A car in good condition",
+  description: "A breif description about the car",
+  location: {
+    district: "Ratnapura",
+    city: "Embilipitya",
+    address: "Kolonna",
+  },
+  contact: {
+    name: "Anjana",
+    phone: "0761234567",
+    email: "hello@anjana784.dev",
+  },
+  negotiable: true,
+  paymentPeriod: "perDay",
 };
 
-const result = AdSchema.safeParse(data);
+const house: PropertyType = {
+  mainCategory: "property",
+  subCategory: "house",
+  title: "A house with 3 bedrooms",
+  description: "A breif description about the house",
+  location: {
+    district: "Ratnapura",
+    city: "Embilipitya",
+    address: "Kolonna",
+  },
+  contact: {
+    name: "Anjana",
+    phone: "0761234567",
+    email: "hello@anjana784.dev",
+  },
+  negotiable: true,
+  paymentPeriod: "perYear",
+};
 
-if (result.success) {
-  console.log("Data is valid", result.data);
+const validatedCar = AdSchema.safeParse(car);
+const validatedHouse = AdSchema.safeParse(house);
+
+if (validatedCar.success) {
+  console.log("Car is valid", validatedCar.data);
 } else {
-  console.log("Data is invalid", result.error.issues[0]);
+  console.log("Car is invalid", validatedCar.error.issues[0]);
+}
+
+if (validatedHouse.success) {
+  console.log("House is valid", validatedHouse.data);
+} else {
+  console.log("House is invalid", validatedHouse.error.issues[0]);
 }
--- src/schema/Ad/index.ts
+++ src/schema/Ad/index.ts
@@ -1,22 +1,93 @@
 import { z } from "zod";
-import { CategorySchema } from "../Category";
 
-const CommonAdSchema = z
+const propertySubCategories = ["house", "apartment"];
+const vehiclesSubCategories = ["car", "motorcycle", "truck"];
+
+const propertySubCategoriesLiteral = ["house", "apartment"] as const;
+const vehicleSubCategoriesLiteral = ["car", "motorcycle", "truck"] as const;
+
+type VehicleSubCategoriesType = (typeof vehicleSubCategoriesLiteral)[number];
+
+type PropertySubCategoriesType = (typeof propertySubCategoriesLiteral)[number];
+
+export const AdSchema = z
   .object({
+    //main category
+    mainCategory: z.enum(["vehicle", "property"]),
+
+    // sub category
+    subCategory: z.enum(["car", "motorcycle", "truck", "house", "apartment"]),
+
+    //ad title
     title: z.string({
       required_error: "Title is required",
       invalid_type_error: "Title should be a string",
     }),
+
+    //ad description
+    description: z.string({
+      required_error: "Description is required",
+      invalid_type_error: "Description should be a string",
+    }),
+
+    //location
+    location: z.object({
+      district: z.string({
+        required_error: "District is required",
+        invalid_type_error: "District should be a string",
+      }),
+      city: z.string({
+        required_error: "City is required",
+        invalid_type_error: "City should be a string",
+      }),
+      address: z.string({
+        required_error: "Address is required",
+        invalid_type_error: "Address should be a string",
+      }),
+    }),
+
+    //images
+    images: z.array(z.string()).optional(),
+
+    //contact
+    contact: z.object({
+      name: z.string({
+        required_error: "Name is required",
+        invalid_type_error: "Name should be a string",
+      }),
+      email: z
+        .string({
+          invalid_type_error: "Email should be a string",
+        })
+        .optional(),
+      phone: z.string({
+        required_error: "Phone is required",
+        invalid_type_error: "Phone should be a string",
+      }),
+    }),
+
+    //negotiable
+    negotiable: z.boolean({
+      required_error: "Negotiable is required",
+      invalid_type_error: "Negotiable should be a boolean",
+    }),
+
+    //payment period
+    paymentPeriod: z.enum([
+      "perYear",
+      "perMonth",
+      "perWeek",
+      "perDay",
+      "perHour",
+    ]),
   })
-  .strict();
-
-export const propertySubCategories = ["house", "apartment"];
-export const vehiclesSubCategories = ["car", "motorcycle", "truck"];
-
-export const AdSchema = CategorySchema.merge(CommonAdSchema).superRefine(
-  (data, ctx) => {
+  .strict()
+  .superRefine((data, ctx: z.RefinementCtx) => {
+    /**
+     * validate sub categories against to each main category
+     */
     // If mainCategory is vehicles, subCategory should be vehicle sub categories
-    if (data.mainCategory === "vehicles") {
+    if (data.mainCategory === "vehicle") {
       if (vehiclesSubCategories.includes(data.subCategory)) {
         return true;
       } else {
@@ -44,7 +115,23 @@ export const AdSchema = CategorySchema.merge(CommonAdSchema).superRefine(
         return z.never();
       }
     }
-  }
-);
+    /**---------------------------------------------------------------------------------------------**/
+  });
 
-export type AdType = z.infer<typeof AdSchema>;
+// get infered type from the ad schema
+type InferedByAdSchema = z.infer<typeof AdSchema>;
+
+//omit main category and sub category
+type WithoutCategories = Omit<
+  InferedByAdSchema,
+  "mainCategory" | "subCategory"
+>;
+
+export type AdType = WithoutCategories &
+  (
+    | { mainCategory: "vehicle"; subCategory: VehicleSubCategoriesType }
+    | { mainCategory: "property"; subCategory: PropertySubCategoriesType }
+  );
+
+export type VehicleType = Extract<AdType, { mainCategory: "vehicle" }>;
+export type PropertyType = Extract<AdType, { mainCategory: "property" }>;
